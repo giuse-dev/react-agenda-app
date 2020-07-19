feb2fcc79eac5dc3e04e98ba517a6f762525ed54
giuse-dev <giuse.developing@gmail.com>
2020-07-19 13:40:34 +0200

    Updates

---
+++ precache-manifest.a8a2aa429f26d209844040e7308132fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f25236635b52e320873764db5e28dc3",
+    "url": "/react-agenda-app/index.html"
+  },
+  {
+    "revision": "8632dfd04e4400bb8031",
+    "url": "/react-agenda-app/static/css/main.0252cc0c.chunk.css"
+  },
+  {
+    "revision": "8953ceb743f31fc17c82",
+    "url": "/react-agenda-app/static/js/2.680819cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-agenda-app/static/js/2.680819cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8632dfd04e4400bb8031",
+    "url": "/react-agenda-app/static/js/main.ea6a1683.chunk.js"
+  },
+  {
+    "revision": "beff9cbbff1185e0fccc",
+    "url": "/react-agenda-app/static/js/runtime-main.65ffb669.js"
+  }
+]);
